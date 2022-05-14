56c01ede2f71c7bff218f630bf3b68e815300ccb
Dinei A. Rockenbach <dineiar@gmail.com>
2022-05-14 01:08:39 -0300

    Ordenação por nome do jogador

--- app/src/app/shared/servicos/jogadores.service.ts
+++ app/src/app/shared/servicos/jogadores.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from './../../../environments/environment';
 
 import { Jogador } from '../modelos/jogador';
@@ -13,7 +14,8 @@ export class JogadoresService {
   constructor(private http: HttpClient) { }
 
   buscarJogadores(): Observable<Jogador[]> {
-    return this.http.get<Jogador[]>(environment.baseurl + this.DADOS);
+    return this.http.get<Jogador[]>(environment.baseurl + this.DADOS)
+      .pipe(map(x => x.sort((a, b) => a.nome.localeCompare(b.nome))));
   }
 
 }
