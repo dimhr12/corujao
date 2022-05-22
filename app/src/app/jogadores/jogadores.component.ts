f8b549cebd71af677e7dd39fc0ebf04a2d6a078e
dimhr12 <dimhr12@gmail.com>
2022-05-22 20:47:35 -0300

    ajustado filtro de jogadores de acordo com a posição

--- app/src/app/jogadores/jogadores.component.ts
+++ app/src/app/jogadores/jogadores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Jogador } from '../shared/modelos/jogador';
 import { JogadoresService } from '../shared/servicos/jogadores.service';
 
 @Component({
@@ -7,14 +8,27 @@ import { JogadoresService } from '../shared/servicos/jogadores.service';
   styleUrls: ['./jogadores.component.scss']
 })
 export class JogadoresComponent implements OnInit {
-
-  public jogadores$ = this.jogadoresService.buscarJogadores();
-
+  public jogadores: Jogador[]=[]; 
   constructor(
     private jogadoresService: JogadoresService,
   ) { }
 
   ngOnInit(): void {
+    this.jogadoresService.buscarJogadores().subscribe((dados)=>{
+      this.jogadores=dados;
+    });
   }
 
+  filtrarPosicao(pos: string) {
+    var jogadoresFiltrados=[];
+
+   // this.jogadores.filter(function(currentValue, index, arr), thisValue)
+
+    for (var i=0; i < this.jogadores.length; i++){
+      if (this.jogadores[i].posicao.includes(pos)) {
+        jogadoresFiltrados.push(this.jogadores[i])
+      }
+    }
+    return jogadoresFiltrados;
+  }
 }
